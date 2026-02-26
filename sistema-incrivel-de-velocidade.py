5bceb863c947db56cc6c2c9edc919f88a5c7c911
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-02-25 21:05:39 -0300

    Eu sou o Homem-Aranha

--- sistema-incrivel-de-velocidade.py
+++ sistema-incrivel-de-velocidade.py
@@ -1,70 +1,64 @@
-#Sistema de Velocidade, calcula média e mostra maior/menor velocidade.
+import time
 
+velocidades = []  # Lista para guardar os valores inseridos
 
-velocidades = [] #Lista para guardar os valores inseridos
-
-
-
-def media(): #função para calc. média;
+def media():  # Função para calc. média;
     b = sum(velocidades) / len(velocidades)
-    print (b)
+    print(b)
 
-def maior(): #função para pegar o maior valor;
+def maior():  # Função para pegar o maior valor;
     a = max(velocidades)
     print(a)
 
-def menor(): #função para pegar o menor valor;
+def menor():  # Função para pegar o menor valor;
     c = min(velocidades)
     print(c)
 
-def soma(): #Função para somar os valores de velocidade;
+def soma():  # Função para somar os valores de velocidade;
     s = sum(velocidades)
     print(s)
 
 print("=== Sistema de Velocidade ===\n")
 while True:
 
-        try: 
-            velocidades.clear() #limpa a lista ao iniciar o programa novamente;
+    try:
+        velocidades.clear()  # Limpa a lista ao iniciar o programa novamente;
 
-            for i in range (5): #laço de repetição;
-                while True:
+        for i in range(5):  # Laço de repetição;
+            while True:
 
-                    velo = int(input("Insira velocidade do motor: ")) #entrada de dados do usuário;
-                    if velo <0: #Condição para não aceita valor < 0;
-                        print("Valor inválido, tente novamente...")
-                    else:
-                        velocidades.append(velo) #Adiciona os valores inseridos à lista;                     
-                        break #Para o looping quando a condição for negada
-    
+                velo = int(input("Insira velocidade do motor: "))  # Entrada de dados do usuário;
+                if velo < 0:  # Condição para não aceitar valor < 0;
+                    print("Valor inválido, tente novamente...")
+                else:
+                    velocidades.append(velo)  # Adiciona os valores inseridos à lista;
+                    break  # Para o looping quando a condição for negada
 
-            
+        print("===Relatório final===\n")
 
+        print("Velocidades inseridas: ", velocidades)
 
-            print("===Relatório final===\n")
+        print("\nMédia Aritmética das velocidades: ")
+        media()
 
-            print("Velocidades inseridas: ", velocidades)
+        print("\nMaior velocidade: ")
+        maior()
 
-            print("\nMédia Aritmética das velocidades: ") 
-            media()
+        print("\nMenor velocidade: ")
+        menor()
 
-            print("\nMaior velocidade: ")
-            maior()
+        print("\nValor total das velocidades: ")
+        soma()
 
-            print("\nMenor velocidade: ")
-            menor()
+        ref = str(input("\nDeseja refazer? s/n: \n"))  # Decisão do usuário de refazer ou não.
 
-            print("\nValor total das velocidades: ")
-            soma()
+        if ref == "s":  # Condição
+            print("Reiniciando programa...")
+            time.sleep(2)  # Aguarda 2 segundos antes de reiniciar
 
-            ref = str(input("\nDeseja refazer? s/n: \n")) #Decisão do usuário de refazer ou não.
-            
-            if ref == "s": #Condição
-                print("Reiniciando programa...")
+        elif ref == "n":
+            print("Programa finalizado.")
+            break  # Para o programa quando a condição for correspondida.
 
-            elif ref == "n":
-                print("Programa finalizado.")
-                break #Para o programa quando a condição for correspondida.
-
-        except ValueError: #Não aceita letras ou diferentes de inteiro.
-            print("Digito inválido, tente novamente...")
+    except ValueError:  # Não aceita letras ou diferentes de inteiro.
+        print("Digito inválido, tente novamente...")
